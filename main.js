5b9afa1168c2db24bb1ac42ee14378cb564b2e50
drafff <i0.0ia@qq.com>
2022-11-03 20:27:34 +0800

    final

--- main.js
+++ main.js
@@ -11,21 +11,33 @@ import {DotScreenShader} from "three/addons/shaders/DotScreenShader.js";
 import {GUI} from "dat.gui";
 import "./style.css"
 import Tween from '@tweenjs/tween.js'
+import {func} from "three/nodes";
 
-//定义变量
+/*
+==============定义变量=================
+*/
 let camera, scene, composer, bloomPass, control, renderer, raycaster
-let clock, model, skeleton, mixer, action, mouse
-let RGBshiftShader, tween, animations, environmentMapTexture, earthenvironmentMapTexture
+let RGBshiftShader, environmentMapTexture, earthenvironmentMapTexture
+//动画相关变量
+let clock, model, skeleton, mixer, action,tween,mouse,animations
+//动画变量，形态状态
 let action1, action2, action3, action4, actionState
+//是否正在变形中
 let actionChanging = true
-
-//资源加载管理
+//画布长宽
+let width = window.innerWidth<window.innerHeight?window.innerWidth:window.innerHeight
+let height = window.innerWidth<window.innerHeight?window.innerWidth:window.innerHeight
+/*
+==============资源加载管理=================
+*/
 const manager = new THREE.LoadingManager();
 manager.onStart = function (url, itemsLoaded, itemsTotal) {
     console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
 };
 //资源加载完成
 manager.onLoad = function () {
+    document.getElementById("loading").remove()
+    //加载完成开始初始化场景
     init()
     console.log('Loading complete!');
 };
@@ -35,52 +47,47 @@ manager.onProgress = function (url, itemsLoaded, itemsTotal) {
 manager.onError = function (url) {
     console.log('There was an error loading ' + url);
 };
+//触摸、点击事件
+window.addEventListener('touchstart', event=>{handlechange(event)})
+window.addEventListener('click', event=>{handlechange(event)})
 
-
-window.addEventListener('click', (event) => {
+function handlechange(event){
     event.preventDefault();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    console.log(event)
+    if(event.type==="touchstart"){
+
+    mouse.x = (event.targetTouches[0].clientX / width) * 2 - 1;
+    mouse.y = -(event.targetTouches[0].clientY / height) * 2 + 1;
+    }else{
+        mouse.x = (event.clientX / width) * 2 - 1;
+        mouse.y = -(event.clientY / height) * 2 + 1;
+    }
+    console.log(mouse)
     raycaster.setFromCamera(mouse, camera);
     const intersection = raycaster.intersectObject(model);
     if (intersection.length > 0 & !actionChanging) {
         actionState = !actionState
         if (actionState) {
             actionChanging = true
-            action1.stop()
-            action2.stop()
-            action3.stop()
             action4.play()
+            action2.stop()
         } else {
             actionChanging = true
-            action1.stop()
-            action2.stop()
             action3.play()
-            action4.stop()
+            action1.stop()
         }
     }
-
-})
-
-// 调试参数
-const params = {
-    RGBshiftShaderp: 0.0015,
-    emissiveIntensity: 1,
-    toneMappingExposure: 1.5,
-    cameraX: 0,
-    cameraY: 40,
-    cameraZ: -2,
 }
 
-//加载资源
-load()
-
+/*
+==============定义函数=================
+*/
 //初始化场景函数
 function init() {
     //获取页面容器位置
     const container = document.getElementById('app');
     //定义相机
-    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 2, 500);
+    camera = new THREE.PerspectiveCamera(40, width / height, 2, 500);
     //初始化相机位置
     camera.position.set(0, 40, -2);
     //初始化相机朝向
@@ -91,13 +98,13 @@ function init() {
     scene = new THREE.Scene();
     raycaster = new THREE.Raycaster();
     mouse = new THREE.Vector2(0, 0)
-    //定义灯光
     //点光
     const pointLight = new THREE.PointLight(0xff2e1f, 1, 10);
     pointLight.position.set(3, 3, 3);
     scene.add(pointLight);
     const pointLight1 = new THREE.PointLight(0x1dbaff, 5, 10);
     pointLight1.position.set(-3, -3, 3);
+    //环境光
     const AmbientLight = new THREE.AmbientLight(0xffffff, 1);
     AmbientLight.position.set(0, 20, 0);
     scene.add(AmbientLight);
@@ -109,14 +116,15 @@ function init() {
         antialias: true
     });
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    //画布尺寸
+    renderer.setSize(width, height);
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.toneMapping = THREE.ACESFilmicToneMapping
     renderer.toneMappingExposure = params.toneMappingExposure
     // 后处理效果
     composer = new EffectComposer(renderer);
     const renderScene = new RenderPass(scene, camera);
-    bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+    bloomPass = new UnrealBloomPass(new THREE.Vector2(width, height), 1.5, 0.4, 0.85);
     RGBshiftShader = new ShaderPass(RGBShiftShader);
     RGBshiftShader.uniforms['amount'].value = params.RGBshiftShaderp;
     const DotScreen = new ShaderPass(DotScreenShader);
@@ -128,16 +136,16 @@ function init() {
     //相机控制器
     control = new OrbitControls(camera, renderer.domElement)
     control.enableDamping = true;
-    control.minDistance = 3;
-    control.maxDistance = 40;
+    control.minDistance = 25;
+    control.maxDistance = 45;
     control.maxPolarAngle = Math.PI * 0.7;
     control.enablePan = false
+    //canve绑定到网页元素
     container.appendChild(renderer.domElement);
     //调试ui
     gui()
     //开场镜头动画
     tween = new Tween.Tween(camera.position)
-
     tween.to({x: -16.8, y: 28.6, z: -24.6}, 1166.67)
         .easing(Tween.Easing.Linear.None)
         .delay(100)
@@ -149,7 +157,6 @@ function init() {
             }, 4333.3)
                 .start()
         })
-
     //场景修改
     scene.add(skeleton);
     scene.add(model);
@@ -164,32 +171,12 @@ function init() {
     model.children[0].children[3].material.envMapIntensity = 0.4
     model.children[0].children[5].children[2].material.envMapIntensity = 5
     scene.background = earthenvironmentMapTexture
-    animate()
+    //开始动画
     tween.start()
     action3.play()
-
-
+    animate()
 }
-
-//调试ui
-function gui() {
-    // const panel = new GUI({width: 300});
-    // panel.add(params, 'RGBshiftShaderp', 0.0, 0.01, 0.0001).onChange(v => {
-    //     RGBshiftShader.uniforms['amount'].value = v
-    // });
-    // panel.add(params, 'emissiveIntensity', 0.0, 1, 0.01).onChange(v => {
-    //     model.children[0].children[4].children[0].material.emissiveIntensity = v
-    //     model.children[0].children[4].children[1].material.emissiveIntensity = v
-    //     model.children[0].children[4].children[2].material.emissiveIntensity = v
-    //     model.children[0].children[1].material.emissiveIntensity = v
-    //     model.children[0].children[2].material.emissiveIntensity = v
-    //     model.children[0].children[3].material.emissiveIntensity = v
-    // });
-    // panel.add(params, 'toneMappingExposure', 0.0, 5, 0.01).onChange(v => {
-    //     renderer.toneMappingExposure = v
-    // });
-}
-
+//资源加载函数
 function load() {
     //材质加载器
     const cubeTextureLoader = new THREE.CubeTextureLoader(manager)
@@ -218,7 +205,7 @@ function load() {
     dracoLoader.setDecoderPath('/draco/');
     loader.setDRACOLoader(dracoLoader);
     //读取模型
-    loader.load('/model/fengzheng2.glb', function (gltf) {
+    loader.load('/model/fengzheng.glb', function (gltf) {
         //读取模型
         model = gltf.scene;
         //读取动画
@@ -234,36 +221,72 @@ function load() {
             const actionName = e.action._clip.name
             if (actionName === 'bianxing-f') {
                 action1.play()
+                action4.stop()
             } else if (actionName === 'bianxing') {
+                // setWeight(2)
                 action2.play()
+                action3.stop()
             }
-            console.log(e.action._clip.name)
         })
-        mixer.addEventListener('loop',e=>{
-            console.log(e)
-        })
-
         //风筝状态
         action1 = mixer.clipAction(animations[0]);
         //天宫状态
         action2 = mixer.clipAction(animations[1]);
         //风筝-》天宫
         action3 = mixer.clipAction(animations[2]);
-        action3.loop = THREE.LoopOnce
+        action3.clampWhenFinished = true
+        action3.loop =  THREE.LoopOnce
         //天宫-》风筝
         action4 = mixer.clipAction(animations[3]);
-        action4.loop = THREE.LoopOnce
-
+        action4.clampWhenFinished = true
+        action4.loop =  THREE.LoopOnce
     });
 }
-
+//动画循环函数
 function animate() {
-    // Render loop
     requestAnimationFrame(animate);
+    // 动画更新
     if (mixer) {
         mixer.update(clock.getDelta())
     }
+    //初始镜头动画
     Tween.update()
     control.update()
     composer.render()
+}
+
+/*
+===============================
+*/
+load()
+
+/*
+==============调试=================
+*/
+// 调试参数（忽视）
+const params = {
+    RGBshiftShaderp: 0.0015,
+    emissiveIntensity: 1,
+    toneMappingExposure: 1.5,
+    cameraX: 0,
+    cameraY: 40,
+    cameraZ: -2,
+}
+//调试ui（忽视）
+function gui() {
+    // const panel = new GUI({width: 300});
+    // panel.add(params, 'RGBshiftShaderp', 0.0, 0.01, 0.0001).onChange(v => {
+    //     RGBshiftShader.uniforms['amount'].value = v
+    // });
+    // panel.add(params, 'emissiveIntensity', 0.0, 1, 0.01).onChange(v => {
+    //     model.children[0].children[4].children[0].material.emissiveIntensity = v
+    //     model.children[0].children[4].children[1].material.emissiveIntensity = v
+    //     model.children[0].children[4].children[2].material.emissiveIntensity = v
+    //     model.children[0].children[1].material.emissiveIntensity = v
+    //     model.children[0].children[2].material.emissiveIntensity = v
+    //     model.children[0].children[3].material.emissiveIntensity = v
+    // });
+    // panel.add(params, 'toneMappingExposure', 0.0, 5, 0.01).onChange(v => {
+    //     renderer.toneMappingExposure = v
+    // });
 }
